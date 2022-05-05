8ed9c0699c84d98e51cc0ba810802fc8a50de698
Ludho <LudhoMP@gmail.com>
2022-05-05 13:05:50 +0430

    using maps

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const {
 } = require("@discordjs/voice");
 const { Client, Intents } = require("discord.js");
 const ora = require("ora");
-const ytdl = require("ytdl-core");
 
 let Bots = new Map();
 Bots.set("javan", {
@@ -29,70 +28,67 @@ Bots.set("pop", {
   type: "Pop",
 });
 
-Bots.forEach(async (bot) => {
-  const botLoader = ora("Starting Discord.js Client").start();
-  const client = new Client({
-    intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES,
-      Intents.FLAGS.GUILD_MEMBERS,
-      Intents.FLAGS.GUILD_VOICE_STATES,
-    ],
-  });
+async function joinChannel(channelId, stream, client) {
+  const voiceJoiner = ora(`${client.user.tag} joining voice channel`);
+  let channel = await client.channels.fetch(channelId);
 
-  client.on("ready", async () => {
-    setInterval(() => {
-      let dragon = ["AVIORA RADIO", "Coded By : Dragon,Ludho"];
-      let Power = Math.floor(Math.random() * dragon.length);
-      client.user.setActivity(dragon[Power], {
-        type: "STREAMING",
-        url: "https://www.twitch.tv/trikanoid",
-      });
-    }, 10000);
-
-    await botLoader.succeed(`${client.user.tag} 24/7 bot is online! `);
-    const voiceJoiner = ora(`${client.user.tag} joining voice channel`);
-
-    await joinChannel(bot.channelId);
-    async function joinChannel(channelId) {
-      let channel = await client.channels.fetch(channelId);
-
+  try {
+    const VoiceConnection = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+    const resource = createAudioResource(stream, {
+      inlineVolume: true,
+    });
+    resource.volume.setVolume(0.2);
+    const player = createAudioPlayer();
+    VoiceConnection.subscribe(player);
+    player.play(resource);
+    player.on("idle", () => {
       try {
-        const VoiceConnection = await joinVoiceChannel({
-          channelId: channel.id,
-          guildId: channel.guild.id,
-          adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-        const resource = await createAudioResource(bot.stream, {
-          inlineVolume: true,
-        });
-        resource.volume.setVolume(0.2);
-        const player = await createAudioPlayer();
-        await VoiceConnection.subscribe(player);
-        await player.play(resource);
-        player.on("idle", () => {
-          try {
-            player.stop();
-          } catch (e) {
-            voiceJoiner.fail(`${client.user.tag} failed to stop the player`);
-            console.error(e);
-          }
-          try {
-            VoiceConnection.destroy();
-          } catch (e) {
-            voiceJoiner.fail(`${client.user.tag} failed to destroy the player`);
-            console.error(e);
-          }
-          joinChannel(channel.id);
-        });
-        await voiceJoiner.succeed(`${client.user.tag} is now playing`);
-      } catch (err) {
-        return (
-          (await voiceJoiner.fail(`${client.user.tag} failed to join`)) +
-          console.error(err)
-        );
+        player.stop();
+      } catch (e) {
+        voiceJoiner.fail(`${client.user.tag} failed to stop the player`);
+        console.error(e);
       }
-    }
-  });
-  client.login(bot.token);
+      try {
+        VoiceConnection.destroy();
+      } catch (e) {
+        voiceJoiner.fail(`${client.user.tag} failed to destroy the player`);
+        console.error(e);
+      }
+      joinChannel(channel.id);
+    });
+    voiceJoiner.succeed(`${client.user.tag} is now playing`);
+  } catch (err) {
+    return (
+      voiceJoiner.fail(`${client.user.tag} failed to join`) + console.error(err)
+    );
+  }
+}
+
+let bot = Bots.get("pop");
+
+const botLoader = ora("Starting Discord.js Client").start();
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
 });
+client.on("ready", async () => {
+  setInterval(() => {
+    let dragon = ["AVIORA RADIO", "Coded By : Dragon,Ludho"];
+    let Power = Math.floor(Math.random() * dragon.length);
+    client.user.setActivity(dragon[Power], {
+      type: "STREAMING",
+      url: "https://www.twitch.tv/trikanoid",
+    });
+  }, 10000);
+  botLoader.succeed(`${client.user.tag} 24/7 bot is online! `);
+  await joinChannel(bot.channelId, bot.stream, client);
+});
+client.login(bot.token);
